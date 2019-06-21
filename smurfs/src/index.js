5e208e23723ffbe312abe5972a50b4d5cd07d63d
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-21 11:14:23 +0200

    Feature: added Router to index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,6 +6,7 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import reducer from './reducers';
 
 const store = createStore(
@@ -14,8 +15,10 @@ const store = createStore(
 );
 
 ReactDOM.render(
+  <Router >
   <Provider store={store}>
     <App />
-  </Provider>,
+  </Provider>
+  </Router>,
   document.getElementById('root')
 );
